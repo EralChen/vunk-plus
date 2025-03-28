1a2801fe46533a08cc7b1f1e3b4e2e5fa7958510
EralChen <eralchen@foxmail.com>
2025-03-28 13:56:36 +0800

    feat: VkAvatar

---
+++ docs/examples/avatar/basic.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { Aim } from '@element-plus/icons-vue'
+import { VkAvatar } from '@vunk-plus/components/avatar'
+</script>
+
+<template>
+  <div sk-flex="row_center">
+    <VkAvatar
+      :icon="Aim"
+      size="small"
+    ></VkAvatar>
+    <span>Icon</span>
+  </div>
+</template>
