8321f6e77cb3e828510336f29541a8a6a6cb7f2c
EralChen <eralchen@foxmail.com>
2025-04-13 23:00:29 +0800

    pref: monaco-environment

--- packages/components/monaco-environment/src/index.vue
+++ packages/components/monaco-environment/src/index.vue
@@ -1,44 +1,42 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import type { NormalObject } from '@vunk/shared'
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-
+import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'VkMonacoEnvironment',
   setup () {
-    
     /**
-     * @description for warning:  
+     * @description for warning:
      * You must define a function MonacoEnvironment.getWorkerUrl or MonacoEnvironment.getWorker？
      */
-    self.MonacoEnvironment = {
-      getWorker (_: string, label: string) {
+    ;(globalThis as NormalObject).MonacoEnvironment = {
+      getWorker (_, label) {
         if (label === 'json') {
-          return new jsonWorker()
+          return new JsonWorker()
         }
         if (label === 'css' || label === 'scss' || label === 'less') {
-          return new cssWorker()
+          return new CssWorker()
         }
         if (label === 'html' || label === 'handlebars' || label === 'razor') {
-          return new htmlWorker()
+          return new HtmlWorker()
         }
         if (['typescript', 'javascript'].includes(label)) {
-          return new tsWorker()
+          return new TsWorker()
         }
         return new EditorWorker()
       },
     }
 
-
-   
     return {}
   },
 })
 </script>
+
 <template>
   <slot></slot>
 </template>
