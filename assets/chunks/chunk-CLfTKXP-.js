3ab3f04e17c6bb1a1134a6c4a53190c6dd4a05cf
EralChen <82485978+EralChen@users.noreply.github.com>
2025-05-13 08:28:47 +0000

    website deploy

--- assets/chunks/chunk-CLfTKXP-.js
+++
@@ -1,1 +0,0 @@
-class t{constructor(){this.promise=new Promise((e,r)=>{this.resolve=e,this.reject=r})}}function o(s){return new Promise(e=>{setTimeout(e,s)})}export{t as D,o as s};
---
+++ assets/chunks/chunk-x6UZKp4a.js
@@ -0,0 +1,11 @@
+import{a as e,t as p,b as m}from"./chunk-CUlHEYfy.js";import{V as a}from"./chunk-TgwF5633.js";import{d as s,Z as n,B as c,h as f}from"./chunk-yZy206vT.js";import"./chunk-DgD4Lv36.js";import"./chunk-Db8R8jrI.js";/* empty css              */import"./chunk-CntOc3u2.js";import"./chunk-DucCsfaH.js";/* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              *//* empty css              */const u=`
+
+大自然里，草长莺飞，莺歌燕舞，她生活在一个美好的世界里。
+
+然而，当她经过茧里的痛苦与挣扎，终于破茧而出时，却不是一只在空中轻盈飞舞的花蝴蝶，而是蜕变成为了一只灰色的小飞蛾。
+
+在同伴的叹息中，她笑着流下了激动的泪水，微笑着说：“上帝给了我生命，就是宝贵的，渺小的我也依然能够在这个美丽的世界中划上一抹绚丽的色彩，做一只用于扑火的飞蛾，又何乐而不为呢？”
+
+没有翅膀的飞翔更接近天堂，有些美丽是不需要书写的。
+
+`,X=s({__name:"basic",setup(_){const r=e({access_token:"5a8e7afe04be860d"}).then(t=>{sessionStorage.setItem("accessToken",t)}),i=async t=>(await r,p({application_id:"ea2c3e52-fa3f-11ef-b7ff-10ffe00db574",text:t}).then(o=>m(o)));return(t,o)=>(c(),n(f(a),{source:u,"text-to-speech":i}))}});export{X as default};
